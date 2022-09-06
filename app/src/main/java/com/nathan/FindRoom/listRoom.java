aa62437108972ea327ac1cc5d596ca123dc3220f
Nathan Tshilay <45175004+nathantshilay@users.noreply.github.com>
2022-09-06 23:17:05 +0200

    initialisation du project

---
+++ app/src/main/java/com/nathan/FindRoom/listRoom.java
@@ -0,0 +1,14 @@
+package com.nathan.FindRoom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_room);
+    }
+}
